15835a1aa112e8cc4fc659c3311fa814b0d0221f
Dekel Livyani <DekelLiv0@gmail.com>
2021-08-18 20:31:48 +0300

    Auto-generated commit [ci skip]

--- src/app/models/move.ts
+++
@@ -1,7 +0,0 @@
-import { Contact } from "./contact";
-export interface Move {
-   _id?: string
-   to: Contact,
-   at: number,
-   amount: number
- }
--- src/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-import { Move } from "./move";
-
-export interface User {
-   _id?: string,
-   name: string,
-   coins: number,
-   moves: Move[]
-}
